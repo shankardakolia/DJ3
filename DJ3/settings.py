41f069dab4d8858e62a50d61559916e2522d2751
SHANKAR DAKOLIA <shankardakolia@gmail.com>
2020-02-21 15:51:36 +0530

    Bootstrap 4

--- DJ3/settings.py
+++ DJ3/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'DJ3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates/',], # <- here
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
